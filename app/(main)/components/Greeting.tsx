b590ac5cd7b1525a459753818192cae6485a6984
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-21 11:00:50 +0700

    :bento: (assets) customize default assets

--- app/(main)/components/Greeting.tsx
+++ app/(main)/components/Greeting.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { Button, Code } from "@heroui/react";
+import { Button, Code, Link } from "@heroui/react";
 import React from "react";
 
 const Greeting = () => {
   return (
     <div className="flex justify-center items-center mt-6 gap-2">
       <Code>Welcome, this is stater kit</Code>
-      <Button color="primary">Click Me</Button>
+      <Button color="primary" as={Link} href="/">
+        Reload Page
+      </Button>
     </div>
   );
 };
