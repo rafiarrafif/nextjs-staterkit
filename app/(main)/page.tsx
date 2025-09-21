79c4b508d300b52b0084e0fe01ecded43d8d9786
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-21 11:26:50 +0700

    :coffin: (conffin) change unoptimalized code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Greeting from "./components/Greeting";
+import Image from "next/image";
 
 const page = () => {
   return (
     <div className="text-center mt-10 text-xl">
+      <div className="h-20 relative w-full flex justify-center">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          fill
+          className="object-contain"
+          draggable={false}
+        />
+      </div>
       <h1>Hello World Page</h1>
       <Greeting />
     </div>
